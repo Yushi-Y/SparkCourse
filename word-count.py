d4005721f4010aa99b9107037f674400cfdea43f
Yushi Yang <54396544+CharlizeY@users.noreply.github.com>
2023-03-02 21:08:39 +0000

    Added new scripts and datasets

---
+++ word-count.py
@@ -0,0 +1,16 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("WordCount")
+sc = SparkContext(conf = conf)
+
+input = sc.textFile("./book.txt")
+
+# Use flatmap() function to split words in the RDD
+words = input.flatMap(lambda x: x.split())
+# countByValue() does not return sorted counts
+wordCounts = words.countByValue()
+
+for word, count in wordCounts.items():
+    cleanWord = word.encode('ascii', 'ignore')
+    if (cleanWord):
+        print(cleanWord.decode() + " " + str(count))
